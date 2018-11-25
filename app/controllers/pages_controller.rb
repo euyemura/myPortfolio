2e5603d86bdbfdf1b6c138499fbbd13b083f8b39
euyemura <ea.uyemura@gmail.com>
2018-11-24 19:55:41 -0800

    added a pages functionality via rails g controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def home
+    @posts = Blog.all
+    # right here we are sending all of the blog posts over to our home page, incredible.
+    # this blog data is available within any of these pages, we just have to call it, in other words, we have to make a database query. 
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
+
+# remember, we dont necessarily need any code here, because when the controlller method has the same name as the view, it'll automatically know that it needs to render the page of that same name as long as it is in a folder that has the same name as the name of the controller.
