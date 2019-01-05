7cfd1483c662a7fb67cacad556898cd13046f0c2
Eric Uyemura <ea.uyemura@gmail.com>
2019-01-05 10:34:18 -0800

    completed authentiacation with virtual attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # these are virtual attributes, it takes a name and splits it, so we must validate the presence of a last_name
+
+  validates_presence_of :name
+
+  def first_name
+      self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
