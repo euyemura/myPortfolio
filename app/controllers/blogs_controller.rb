442b0e299a60eaf32a332443e70d965ddde9544c
Eric Uyemura <ea.uyemura@gmail.com>
2019-02-05 14:15:03 -0800

    created partials for blog, fontawesome, and pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.special_blogs
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "Blog | Eric Akio"
     @seo_keywords = "Eric Uyemura portfolio blog"
   end
