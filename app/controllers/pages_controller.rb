d9f9f28b009dbe8e7b34b380f006cac7b2bd30de
Eric Uyemura <ea.uyemura@gmail.com>
2019-02-07 19:03:42 -0800

    implemented twitter call on mindful news page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     @posts = Blog.all
     # right here we are sending all of the blog posts over to our home page, incredible.
     # this blog data is available within any of these pages, we just have to call it, in other words, we have to make a database query.
-    # we're essentially showing how to get data out of one controller, model, and view to another set of them, all within the same application which is pretty awesome. 
+    # we're essentially showing how to get data out of one controller, model, and view to another set of them, all within the same application which is pretty awesome.
     @skills = Skill.all
   end
 
@@ -11,6 +11,10 @@ class PagesController < ApplicationController
   end
 
   def contact
+  end
+
+  def mindful_news
+    @tweets = SocialTool.twitter_search
   end
 end
 
