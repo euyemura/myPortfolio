31db3016d6ff07da3126eb46c02b5e7847971fb8
Eric Uyemura <ea.uyemura@gmail.com>
2019-02-16 10:56:16 -0800

    integrated production calls for action cableg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://devcamp-portfolio.herokuapp.com', 'http://devcamp-portfolio.herokuapp.com', 'https://www.akio.life', 'https://akio.life']
+  config.action_cable.url = "wss://devcamp-portfolio.herokuapp.com/cable"
 end
